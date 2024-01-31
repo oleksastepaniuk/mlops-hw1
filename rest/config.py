461580eba14d4610f27cfb7fa5bd564b25621c9b
Oleksa Stepaniuk <stepaniuq@gmail.com>
2024-01-31 20:38:02 +0200

    added config class

---
+++ rest/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    POSTGRES_USER = os.environ.get('POSTGRES_USER')
+    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+    POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+    POSTGRES_DB = os.environ.get('POSTGRES_DB')
