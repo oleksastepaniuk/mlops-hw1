c06943b538108af29679efac0a8ad1f66c758b38
Oleksa Stepaniuk <stepaniuq@gmail.com>
2024-02-04 23:25:56 +0200

    question service

--- rest/config.py
+++ rest/config.py
@@ -1,8 +1,4 @@
 import os
 
 class Config:
-    POSTGRES_USER = os.environ.get('POSTGRES_USER')
-    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-    POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
-    POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
-    POSTGRES_DB = os.environ.get('POSTGRES_DB')
+    POSTGRES_DB_URL = os.environ.get('POSTGRES_DB_URL')
--- rest/db/migration/V1__Intitial_Schema.sql
+++ rest/db/migration/V1__Intitial_Schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE questions(
     author_id UUID NOT NULL,
     body TEXT NOT NULL
 );
+
 CREATE TABLE answers(
     id UUID PRIMARY KEY,
     author_id UUID NOT NULL,
@@ -10,3 +11,10 @@ CREATE TABLE answers(
     score INT NOT NULL,
     body TEXT NOT NULL
 );
+
+ALTER TABLE answers
+ADD CONSTRAINT fk_question_id
+FOREIGN KEY (question_id) REFERENCES questions(id);
+
+CREATE INDEX idx_author_id_questions ON questions(author_id);
+CREATE INDEX idx_author_id_answers ON answers(author_id);
